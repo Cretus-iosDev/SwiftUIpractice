dc4030583280bb49ce09e74fa06e8ab84909744f
Cretus-iosDev <cretus.iosdev@gmail.com>
2024-04-08 09:14:56 +0530

    Initial Commit

---
+++ SwiftUIpractice/SwiftUIpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIpracticeApp.swift
+//  SwiftUIpractice
+//
+//  Created by Dr. Shrikant Maraskolhe on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
